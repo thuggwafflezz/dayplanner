067d9727f062bea6ef09f8c1af2162ce9a599f65
Javier Delcid <javierldelcid@gmail.com>
2022-01-03 19:49:38 -0800

    add code

--- assets/logic.js
+++ assets/logic.js
@@ -16,7 +16,7 @@ function displayTime(){
     timeDisplayEl.text(today)
 }
 
-//set current hour
+/set current hour
 var currentHour = moment().hour()
 //determine block time and check block time against current time
 $('.time-block').each(function(){
@@ -40,3 +40,23 @@ $('.time-block').each(function(){
         $(this).removeClass('future')
         $(this).addClass('present')
     }
+
+})
+setInterval(displayTime, 1000)
+
+//pulls objects from localstorage if there are any
+$(document).ready(function(){
+    $('#hour8 textarea').val(localStorage.getItem('hour8'))
+    $('#hour9 textarea').val(localStorage.getItem('hour9'))
+    $('#hour10 textarea').val(localStorage.getItem('hour10'))
+    $('#hour11 textarea').val(localStorage.getItem('hour11'))
+    $('#hour12 textarea').val(localStorage.getItem('hour12'))
+    $('#hour13 textarea').val(localStorage.getItem('hour13'))
+    $('#hour14 textarea').val(localStorage.getItem('hour14'))
+    $('#hour15 textarea').val(localStorage.getItem('hour15'))
+    $('#hour16 textarea').val(localStorage.getItem('hour16'))
+    $('#hour17 textarea').val(localStorage.getItem('hour17'))
+
+})
+
+
